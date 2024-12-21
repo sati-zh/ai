3398fc2966158abe4c2446e439e415e36e9e005f
sati-zh <zhagupova.sati@gmail.com>
2024-12-21 12:31:31 +0300

    .

--- sklearn.py
+++ sklearn.py
@@ -3,25 +3,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
-# Загрузка данных о винах
 wine_data = datasets.load_wine()
-features = wine_data.data.tolist()  # Характеристики вин в виде списка
-labels = wine_data.target.tolist()   # Типы вин в виде списка
+features = wine_data.data.tolist()  
+labels = wine_data.target.tolist()  
 
-# Разделение данных на обучающую и тестовую выборки
 train_features, test_features, train_labels, test_labels = train_test_split(
     features, labels, test_size=0.2, random_state=42
 )
 
-# Создание и обучение модели классификации
-model = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)  # Многослойный перцептрон
-model.fit(train_features, train_labels)  # Обучение модели
+#создание и обучение модели классификации
+model = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)  
+model.fit(train_features, train_labels)  # Обучение
 
-# Прогнозирование на тестовых данных
+#прогнозирование
 predicted_labels = model.predict(test_features)
 
-# Вычисление точности
+#точности
 precision = accuracy_score(test_labels, predicted_labels)
 
-# Вывод результата
+#результат
 print(f'Точность классификации: {precision:.2f}')
